985fc404dbca6a7032c401d20045d94238a6a47f
Juan Pablo Levy <operaciones@indasoge.com>
2023-12-20 09:39:48 -0300

    [ADD] creating requeriments.txt and a base module l10n_cl_dependencies  to have pdf417gen in the project for migration

---
+++ l10n_cl_dependencies/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_cl_dependencies",
+
+    'summary': """
+        Add External depency on pdf417gen for migrations""",
+
+    'description': """
+        Add External depency on pdf417gen for migrations
+    """,
+
+    'author': "Indasoge SpA",
+    'website': "http://www.indasoge.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # external depency on pdf417gen
+    'external_dependencies': {'python': ['pdf417gen']},
+    
+}
