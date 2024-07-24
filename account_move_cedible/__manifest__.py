2754e8e5c6bf9f9e1fe35faff95897b6d4385424
CarlosFULLHD <cninareynaga@gmail.com>
2024-07-24 15:50:39 -0400

    [FIX] ValueError in custom_header by inheriting template and handling document number as string in account_move_cedible module

--- account_move_cedible/__manifest__.py
+++ account_move_cedible/__manifest__.py
@@ -9,7 +9,8 @@
 	'depends': ['base', 'account','l10n_cl','l10n_cl_edi'],
 	'data': [
 		'security/ir.model.access.csv',
-		'views/template_cedible.xml',		
+		'views/template_cedible.xml',	
+                                'views/template_cedible_inherit.xml',	
 	],
 	'installable': True,
 	"sequence": 10,
